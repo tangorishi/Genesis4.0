d7d8373258bfde38711df71f1d29cd51cedb5674
Apratim Pandey <apratxm@gmail.com>
2024-07-27 23:20:45 +0530

    carousel and grid responsiveness <sm

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,34 +1,51 @@
 import Link from "next/link";
 import { InstagramLogoIcon } from "@radix-ui/react-icons";
-import genesisLogo from './../../public/footer/WHITE TEXT .png';
-import ieeeSBLogo from './../../public/footer/ieee sb white logo.png';
-import ieeeCSLogo from './../../public/footer/IEEE CS WHITE LOGO.png';
-import ieeeWIELogo from './../../public/footer/ieee wie white logo.png';
+import genesisLogo from "./../../public/footer/WHITE TEXT .png";
+import ieeeSBLogo from "./../../public/footer/ieee sb white logo.png";
+import ieeeCSLogo from "./../../public/footer/IEEE CS WHITE LOGO.png";
+import ieeeWIELogo from "./../../public/footer/ieee wie white logo.png";
 
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-b from-blue-900 via-blue-800 to-blue-500 h-auto flex flex-col px-4 md:px-10 xl:px-28 overflow-hidden p-8"
+    <div
+      className="bg-gradient-to-b from-blue-900 via-blue-800 to-blue-500 h-auto flex flex-col px-4 md:px-10 xl:px-28 overflow-hidden p-8"
       style={{
         backgroundImage: "url('/footer/bgfooter.svg')",
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
       }}
     >
       <div className="flex flex-col lg:flex-row justify-between items-center">
         <div className="flex flex-col items-start mb-8 lg:mb-0 lg:w-1/3">
-          <img src={genesisLogo.src} alt="Genesis Logo" className="w-80 h-auto mb-4" />
+          <img
+            src={genesisLogo.src}
+            alt="Genesis Logo"
+            className="w-80 h-auto mb-4"
+          />
           <div className="flex items-center gap-4">
-          <img src={ieeeCSLogo.src} alt="IEEE CS Logo" className="h-12 w-auto" />
-            <img src={ieeeSBLogo.src} alt="IEEE SB Logo" className="h-12 w-auto" />
-            <img src={ieeeWIELogo.src} alt="IEEE WIE Logo" className="h-12 w-auto" />
+            <img
+              src={ieeeCSLogo.src}
+              alt="IEEE CS Logo"
+              className="h-12 w-auto"
+            />
+            <img
+              src={ieeeSBLogo.src}
+              alt="IEEE SB Logo"
+              className="h-12 w-auto"
+            />
+            <img
+              src={ieeeWIELogo.src}
+              alt="IEEE WIE Logo"
+              className="h-12 w-auto"
+            />
           </div>
         </div>
 
         <div className="hidden lg:block w-px bg-white h-40 mx-4"></div>
 
         <div className="flex flex-col items-center justify-center text-white lg:w-1/3 lg:px-4">
-        <ContactInfo
+          <ContactInfo
             name="Rishi Joshi"
             phone="+91 95699 13103"
             role="(Chairperson-IEEE CS MUJ)"
@@ -51,17 +68,32 @@ const Footer = () => {
         <div className="flex flex-col items-center lg:items-end text-white mt-8 lg:mt-0 lg:w-1/3 lg:px-4">
           <WebsiteLink name="IEEE CS WEBSITE" href="https://cs.ieeemuj.com/" />
           <WebsiteLink name="IEEE SB WEBSITE" href="https://ieeemuj.com/" />
-          <WebsiteLink name="IEEE WIE WEBSITE" href="https://wie.ieeemuj.com/" />
+          <WebsiteLink
+            name="IEEE WIE WEBSITE"
+            href="https://wie.ieeemuj.com/"
+          />
         </div>
       </div>
 
       <div className="w-full h-px bg-white my-8"></div>
 
       <div className="flex justify-center items-center space-x-8">
-        <InstagramLink name="Genesis" href="https://www.instagram.com/genesismuj/" />
-        <InstagramLink name="IEEE CS" href="https://www.instagram.com/ieeecs_muj/" />
-        <InstagramLink name="IEEE SB" href="https://www.instagram.com/ieeemuj/?igshid=NjIwNzIyMDk2Mg%3D%3D" />
-        <InstagramLink name="IEEE WIE" href="https://www.instagram.com/ieee.wiemuj/?igshid=NjIwNzIyMDk2Mg%3D%3D" />
+        <InstagramLink
+          name="Genesis"
+          href="https://www.instagram.com/genesismuj/"
+        />
+        <InstagramLink
+          name="IEEE CS"
+          href="https://www.instagram.com/ieeecs_muj/"
+        />
+        <InstagramLink
+          name="IEEE SB"
+          href="https://www.instagram.com/ieeemuj/?igshid=NjIwNzIyMDk2Mg%3D%3D"
+        />
+        <InstagramLink
+          name="IEEE WIE"
+          href="https://www.instagram.com/ieee.wiemuj/?igshid=NjIwNzIyMDk2Mg%3D%3D"
+        />
       </div>
     </div>
   );
@@ -76,13 +108,19 @@ const ContactInfo = ({ name, phone, role }) => (
 );
 
 const WebsiteLink = ({ name, href }) => (
-  <Link href={href} className="flex items-center text-white mb-4 hover:underline">
+  <Link
+    href={href}
+    className="flex items-center text-white mb-4 hover:underline"
+  >
     {name}
   </Link>
 );
 
 const InstagramLink = ({ name, href }) => (
-  <Link href={href} className="flex items-center text-white hover:text-gray-300 transition-colors">
+  <Link
+    href={href}
+    className="flex items-center text-white hover:text-gray-300 transition-colors"
+  >
     <InstagramLogoIcon className="w-6 h-6 mr-2" />
     <span>{name}</span>
   </Link>
--- components/ImageGrid/index.jsx
+++ components/ImageGrid/index.jsx
@@ -45,38 +45,38 @@ const GridImage = () => {
         />
       </div>
 
-      <div className="flex flex-row gap-x-8 lg:gap-x-8 max-w-[700px] lg:max-w-full mx-auto justify-between lg:m-0 sm:gap-x-6">
-        <div className="flex flex-col lg:gap-y-10 gap-y-8">
+      <div className="flex flex-row lg:gap-x-8 max-w-[700px] lg:max-w-full mx-auto justify-between lg:m-0 sm:gap-x-6">
+        <div className="flex flex-col lg:gap-y-10 gap-y-8 flex-wrap sm:flex-nowrap">
           <Image
             src={img6}
             alt="1"
-            className="flex:1 2xl:flex-none 2xl:w-[420px] 2xl:h-[300px] lg:h-[550px] w-full h-[500px] rounded-3xl object-cover lg:w-max-[700px]"
+            className="flex:1 2xl:flex-none 2xl:w-[420px] 2xl:h-[300px] lg:h-[550px] w-full h-[250px] sm:h-[500px] rounded-3xl object-cover lg:w-max-[700px]"
             placeholder="blur"
           />
           <Image
             src={img6}
             alt="1"
-            className="hidden 2xl:block 2xl:w-[420px] 2xl:h-[300px] lg:h-[250px] lg:w-[280px] w-[280px] h-[200px] rounded-3xl object-cover"
+            className="block sm:hidden 2xl:block 2xl:w-[420px] 2xl:h-[300px] lg:h-[250px] lg:w-[280px] h-[250px] rounded-3xl object-cover"
             placeholder="blur"
           />
         </div>
         <Image
           src={img6}
           alt="1"
-          className="sm:hidden 2xl:block 2xl:flex-1 2xl:h-[650px] 2xl:w-[716px] lg:h-[550px] lg:w-[400px] w-[280px] h-[200px] rounded-3xl object-cover"
+          className="hidden 2xl:block 2xl:flex-1 2xl:h-[650px] 2xl:w-[716px] lg:h-[550px] lg:w-[400px] w-[280px] h-[200px] rounded-3xl object-cover"
           placeholder="blur"
         />
         <div className="flex flex-col lg:gap-y-10 gap-y-8">
           <Image
             src={img6}
             alt="1"
-            className="lg:flex-1 2xl:flex-none 2xl:block lg:h-[550px] lg:w-[550px] 2xl:w-[420px] 2xl:h-[300px] w-[300px] h-[230px] rounded-3xl object-cover"
+            className="hidden sm:block lg:flex-1 2xl:flex-none 2xl:block lg:h-[550px] lg:w-[550px] 2xl:w-[420px] 2xl:h-[300px] w-[300px] h-[230px] rounded-3xl object-cover"
             placeholder="blur"
           />
           <Image
             src={img6}
             alt="1"
-            className=" lg:hidden 2xl:block 2xl:w-[420px] 2xl:h-[300px] lg:h-[250px] lg:w-[280px] w-[300px] h-[230px] rounded-3xl object-cover"
+            className="hidden sm:block lg:hidden 2xl:block 2xl:w-[420px] 2xl:h-[300px] lg:h-[250px] lg:w-[280px] w-[300px] h-[230px] rounded-3xl object-cover"
             placeholder="blur"
           />
         </div>
@@ -154,9 +154,9 @@ const Carousel = () => {
   }, []);
 
   return (
-    <div className="mt-8 lg:mt-12 slider relative w-full h-[320px] md:h-[350px] lg:h-[470px] xl:h-[570px] 2xl:h-[650px] flex items-center justify-center overflow-hidden">
+    <div className="mt:4 sm:mt-8 lg:mt-12 slider relative w-full h-[400px] sm:h-[320px] md:h-[350px] lg:h-[470px] xl:h-[570px] 2xl:h-[650px] flex items-center justify-center overflow-hidden">
       <button
-        className={`absolute next sm:text-[150px] md:text-[200px] lg:text-[250px] xl:text-[270px] 2xl:text-[350px] text-customArrow bg-transparent border-none text-[250px] font-bold inline-block left-[30px] md:left-[10px] xl:left-[50px] 2xl:left-[50=px] ${styles["text-shadow-md"]}`}
+        className={`absolute h-[30px]  bottom-0 left-12 pb-12 text-[50px] sm:bottom-auto sm:p-0 sm:h-fit sm:text-[150px] md:text-[200px] lg:text-[250px] xl:text-[270px] 2xl:text-[350px] text-customArrow bg-transparent border-none font-bold inline-block sm:left-[30px] md:left-[10px] xl:left-[50px] 2xl:left-[50=px] ${styles["text-shadow-md"]}`}
         id="previous"
       >
         &lt;
@@ -165,26 +165,26 @@ const Carousel = () => {
       <Image
         src={img3}
         alt="1"
-        className={`picture absolute transform -translate-y-1/2 rounded-3xl h-[270px] md:h-[300px] lg:h-[400px] xl:h-[520px] 2xl:h-[600px] ${styles["image-glow"]} w-fit`}
+        className={`picture mb-4 sm:mb-0 absolute transform -translate-y-1/2 rounded-3xl h-[250px] sm:h-[270px] md:h-[300px] lg:h-[400px] xl:h-[520px] 2xl:h-[600px] ${styles["image-glow"]} w-fit`}
       />
       <Image
         src={img4}
         alt="2"
-        className={`picture absolute  -translate-y-1/2 rounded-3xl h-[270px] md:h-[300px] lg:h-[400px] xl:h-[520px] 2xl:h-[600px] ${styles["image-glow"]} w-fit`}
+        className={`picture mb-4 sm:mb-0 absolute  -translate-y-1/2 rounded-3xl h-[250px] sm:h-[270px] md:h-[300px] lg:h-[400px] xl:h-[520px] 2xl:h-[600px] ${styles["image-glow"]} w-fit`}
       />
       <Image
         src={img3}
         alt="3"
-        className={`picture absolute -translate-y-1/2 rounded-3xl h-[270px] md:h-[300px] lg:h-[400px] xl:h-[520px] 2xl:h-[600px] ${styles["image-glow"]} w-fit`}
+        className={`picture mb-4 sm:mb-0 absolute -translate-y-1/2 rounded-3xl h-[250px] sm:h-[270px] md:h-[300px] lg:h-[400px] xl:h-[520px] 2xl:h-[600px] ${styles["image-glow"]} w-fit`}
       />
       <Image
         src={img4}
         alt="4"
-        className={`picture absolute -translate-y-1/2 rounded-3xl h-[270px] md:h-[300px] lg:h-[400px] xl:h-[520px] 2xl:h-[600px] w-fit ${styles["image-glow"]}`}
+        className={`picture mb-4 sm:mb-0 absolute -translate-y-1/2 rounded-3xl h-[250px] sm:h-[270px] md:h-[300px] lg:h-[400px] xl:h-[520px] 2xl:h-[600px] w-fit ${styles["image-glow"]}`}
       />
 
       <button
-        className={`sm:text-[150px] md:text-[200px] lg:text-[250px] xl:text-[250px] 2xl:text-[350px] absolute text-customArrow bg-transparent border-none text-[250px] font-bold inline-block right-[30px] md:right-[10px] xl:right-[50px] 2xl:right-[50px] ${styles["text-shadow-md"]}`}
+        className={`absolute h-[30px]  bottom-0 right-12 pb-12 text-[50px] sm:bottom-auto sm:p-0 sm:h-fit sm:text-[150px] md:text-[200px] lg:text-[250px] xl:text-[250px] 2xl:text-[350px] text-customArrow bg-transparent border-none font-bold inline-block sm:right-[30px] md:right-[10px] xl:right-[50px] 2xl:right-[50px] ${styles["text-shadow-md"]}`}
         id="next"
       >
         &gt;
