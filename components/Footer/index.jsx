5ec4778f536c360375f2db26ca4e46381f14cd5e
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-08-03 00:18:21 +0530

    Footer responsiveness fix

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -17,13 +17,13 @@ const Footer = () => {
       }}
     >
       <div className="flex flex-col lg:flex-row justify-between items-center">
-        <div className="flex flex-col items-start mb-8 lg:mb-0 lg:w-1/3">
+        <div className="flex flex-col items-center mb-8 lg:mb-0 lg:w-1/3">
           <img
             src={genesisLogo.src}
             alt="Genesis Logo"
             className="w-80 h-auto mb-4"
           />
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-4 flex-col sm:flex-row">
             <img
               src={ieeeCSLogo.src}
               alt="IEEE CS Logo"
@@ -68,16 +68,13 @@ const Footer = () => {
         <div className="flex flex-col items-center lg:items-end text-white mt-8 lg:mt-0 lg:w-1/3 lg:px-4">
           <WebsiteLink name="IEEE CS WEBSITE" href="https://cs.ieeemuj.com/" />
           <WebsiteLink name="IEEE SB WEBSITE" href="https://ieeemuj.com/" />
-          <WebsiteLink
-            name="IEEE WIE WEBSITE"
-            href="https://wie.ieeemuj.com/"
-          />
+          <WebsiteLink name="IEEE WIE WEBSITE" href="https://wie.ieeemuj.com/" />
         </div>
       </div>
 
       <div className="w-full h-px bg-white my-8"></div>
 
-      <div className="flex justify-center items-center space-x-8">
+      <div className="flex justify-center items-center flex-wrap">
         <InstagramLink
           name="Genesis"
           href="https://www.instagram.com/genesismuj/"
@@ -119,7 +116,7 @@ const WebsiteLink = ({ name, href }) => (
 const InstagramLink = ({ name, href }) => (
   <Link
     href={href}
-    className="flex items-center text-white hover:text-gray-300 transition-colors"
+    className="flex items-center text-white hover:text-gray-300 transition-colors w-1/2 pl-[16%] sm:pl-0 sm:w-1/4 sm:justify-center"
   >
     <InstagramLogoIcon className="w-6 h-6 mr-2" />
     <span>{name}</span>
