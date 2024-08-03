17c0f810ea46cecec3df93eb0b9b0f78868ad098
Kanishka Bhatia <bhatiakanishka930@gmail.com>
2024-08-03 17:07:15 +0530

    responsiveness added

--- components/EventCard/index.jsx
+++ components/EventCard/index.jsx
@@ -8,8 +8,8 @@ function EventCard() {
       <DialogTrigger>
         <div className="flex justify-center items-center">
           <div
-            className="flex flex-col justify-start items-center
-            md:w-[25rem] md:h-[45rem] gap-4 md:gap-6 cursor-pointer rounded-[4px] border
+            className="flex flex-col justify-start items-center w-[18rem] h-[32rem] sm:w-[15rem] sm:h-[30rem]
+            md:w-[16rem] md:h-[32rem] xl:w-[18rem] xl:h-[35rem] gap-4 md:gap-6 cursor-pointer rounded-[4px] border
             border-[#E5FFF1] transition-transform duration-200 ease-in-out"
             style={{
               background:
@@ -25,7 +25,7 @@ function EventCard() {
                 />
               </div>
               <p
-                className="font-medium text-[3.5rem] md:text-[2.5rem]"
+                className="font-medium text-[1.5rem] sm:text-[1.5rem] md:text-[1.75rem]"
                 style={{
                   background:
                     "linear-gradient(80deg, #6CC6EF 6.67%, #72CFFA 28.13%, #74D3FF 64.87%, #6CC6EF 95.66%)",
@@ -36,12 +36,12 @@ function EventCard() {
               >
                 Breaking Bug
               </p>
-              <p className="text-white text-[1.5rem] md:text-[1.1rem]">
+              <p className="text-white text-[0.85rem] sm:text-[0.85rem] md:text-[1rem]">
                 Lorem ipsum dolor sit amet consectetur{" "}
               </p>
             </div>
 
-            <div className="flex gap-4 mt-0 pb-4 text-[1.75rem] md:text-[1rem]">
+            <div className="flex gap-4 mt-0 pb-4 text-[0.85rem] sm:text-[0.85rem] md:text-[1rem]">
               <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors duration-200">
                 Know more
               </button>
