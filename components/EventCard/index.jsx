39eb76880a7132dfba5f3ea6e29389c82d541897
AwesomeSam9523 <samakshgupta04@gmail.com>
2024-09-29 21:02:00 +0530

    fix

--- components/EventCard/index.jsx
+++ components/EventCard/index.jsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from "next/image";
 import {useRouter} from "next/navigation";
-import Link from "next/Link";
+import Link from "next/link";
 
 function EventCard({event}) {
   const router = useRouter();
